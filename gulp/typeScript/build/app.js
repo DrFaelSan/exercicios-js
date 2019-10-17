f63b50370ed7883a60f7ce1f934eb199e7fa10ed
DrFaelSan <rafaelvplima@gmail.com>
2019-10-17 17:49:29 -0300

    decimo segundo commit

--- gulp/typeScript/build/app.js
+++ gulp/typeScript/build/app.js
@@ -4,9 +4,9 @@ var produto_1 = require("./produto");
 function exibir(v) {
     console.log(v.nome + " custa " + v.preco + "!");
 }
-var c = new produto_1.Carro();
-c.nome = "Civic";
-c.preco = 89499.0;
+var c = new produto_1.Carro;
+c.nome = 'Civic';
+c.preco = 89499.00;
 exibir(c);
 function soma(a, b) {
     return a + b;
