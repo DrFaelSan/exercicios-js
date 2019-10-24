519c1457326ce13fdd3ef73504da89ecab3167a2
DrFaelSan <rafaelvplima@gmail.com>
2019-10-24 16:15:15 -0300

    decimo quinto commit

---
+++ react/crud/frontend/src/components/template/Footer.jsx
@@ -0,0 +1,11 @@
+import "./Footer.css";
+import React from "react";
+
+export default props => (
+  <footer className="footer">
+    <span>
+      Desenvolvido com por
+      <strong className="text-success"> Rafael</strong>
+    </span>
+  </footer>
+);
